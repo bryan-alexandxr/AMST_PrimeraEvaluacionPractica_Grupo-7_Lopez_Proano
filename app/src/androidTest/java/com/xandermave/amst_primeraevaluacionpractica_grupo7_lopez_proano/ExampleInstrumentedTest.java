0e83f89c4d32aae05cbe9bb134bca560b821b8cb
bryan-alexandxr <balopez@espol.edu.ec>
2022-11-25 16:09:09 -0500

    Commit Inicial

---
+++ app/src/androidTest/java/com/xandermave/amst_primeraevaluacionpractica_grupo7_lopez_proano/ExampleInstrumentedTest.java
@@ -0,0 +1,25 @@
+package com.xandermave.amst_primeraevaluacionpractica_grupo7_lopez_proano;
+
+import android.content.Context;
+import android.support.test.InstrumentationRegistry;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.*;
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+@RunWith(AndroidJUnit4.class)
+public class ExampleInstrumentedTest {
+    @Test
+    public void useAppContext() {
+        // Context of the app under test.
+        Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
+        assertEquals("com.xandermave.amst_primeraevaluacionpractica_grupo7_lopez_proano", appContext.getPackageName());
+    }
+}
---
+++ app/src/test/java/com/xandermave/amst_primeraevaluacionpractica_grupo7_lopez_proano/ExampleUnitTest.java
@@ -0,0 +1,17 @@
+package com.xandermave.amst_primeraevaluacionpractica_grupo7_lopez_proano;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() {
+        assertEquals(4, 2 + 2);
+    }
+}
