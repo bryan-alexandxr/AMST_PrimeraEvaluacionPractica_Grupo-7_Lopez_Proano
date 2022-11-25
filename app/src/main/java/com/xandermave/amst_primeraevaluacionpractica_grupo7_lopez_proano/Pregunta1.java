afc4e8ed93cf0839667c35e7eb6ae464929443bd
bryan-alexandxr <balopez@espol.edu.ec>
2022-11-25 16:24:34 -0500

    Creacion de activities para toda la app

---
+++ app/src/main/java/com/xandermave/amst_primeraevaluacionpractica_grupo7_lopez_proano/Pregunta1.java
@@ -0,0 +1,13 @@
+package com.xandermave.amst_primeraevaluacionpractica_grupo7_lopez_proano;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Pregunta1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pregunta1);
+    }
+}
---
+++ app/src/main/java/com/xandermave/amst_primeraevaluacionpractica_grupo7_lopez_proano/Pregunta5.java
@@ -0,0 +1,13 @@
+package com.xandermave.amst_primeraevaluacionpractica_grupo7_lopez_proano;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Pregunta5 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pregunta5);
+    }
+}
