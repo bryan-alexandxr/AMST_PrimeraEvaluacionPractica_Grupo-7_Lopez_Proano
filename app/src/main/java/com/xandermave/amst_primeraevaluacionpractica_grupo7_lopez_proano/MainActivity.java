6dbb0e5f742e9d3a790515d7127c5fcad39b1afc
bryan-alexandxr <balopez@espol.edu.ec>
2022-11-25 16:25:32 -0500

    prueba

--- app/src/main/java/com/xandermave/amst_primeraevaluacionpractica_grupo7_lopez_proano/MainActivity.java
+++ app/src/main/java/com/xandermave/amst_primeraevaluacionpractica_grupo7_lopez_proano/MainActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    //Main activity es la Bienvenida
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
