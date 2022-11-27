4463d8a679888eb75b3d32cfab742c70dc19ac9e
MaverickProano <jmproano@espol.edu.ec>
2022-11-26 19:49:33 -0500

    Nuevos Cambios

--- app/src/main/java/com/xandermave/amst_primeraevaluacionpractica_grupo7_lopez_proano/Derrota.java
+++ app/src/main/java/com/xandermave/amst_primeraevaluacionpractica_grupo7_lopez_proano/Derrota.java
@@ -1,5 +1,6 @@
 package com.xandermave.amst_primeraevaluacionpractica_grupo7_lopez_proano;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -8,19 +9,27 @@ import android.widget.TextView;
 
 public class Derrota extends AppCompatActivity {
 
-    TextView numero;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_derrota);
         Button salir = (Button) findViewById(R.id.btnSalir);
-        TextView numero = findViewById(R.id.txtNumeroP);
+        Button volver = (Button) findViewById(R.id.button);
+        TextView numero = (TextView) findViewById(R.id.Numero);
         salir.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 finish();
-                // numero.setText(cont);
+                numero.setText("cont");
+            }
+        });
+        Intent i = new Intent(this, MainActivity.class);
+        volver.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                startActivity(i);
             }
         });
     }
